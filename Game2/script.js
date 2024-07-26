4dcb073265dfc0beb5673039d9d3a027564f3b00
Claudio <78203483+cdigrego@users.noreply.github.com>
2024-07-26 14:12:01 +0200

    Add files via upload

--- Game2/script.js
+++ Game2/script.js
@@ -15,11 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Labirinto di esempio
     const walls = [
+	 // Primo quadrato
     { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 },
     { x: 1, y: 2 }, { x: 4, y: 2 },
     { x: 1, y: 3 }, { x: 4, y: 3 },
     { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 }, { x: 4, y: 4 },
-    { x: 6, y: 1 }, { x: 7, y: 1 }, { x: 8, y: 1 },
+	  // Secondo quadrato
+    { x: 6, y: 1 }, { x: 8, y: 1 },
     { x: 6, y: 2 }, { x: 8, y: 2 },
     { x: 6, y: 3 }, { x: 8, y: 3 },
     { x: 6, y: 4 }, { x: 7, y: 4 }, { x: 8, y: 4 },
