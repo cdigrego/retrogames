777de61048c5d82ad7e282e1cf047fe63c923354
Claudio <78203483+cdigrego@users.noreply.github.com>
2024-07-26 14:16:55 +0200

    Add files via upload

--- Game2/script.js
+++ Game2/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 },
     { x: 1, y: 2 }, { x: 4, y: 2 },
     { x: 1, y: 3 }, { x: 4, y: 3 },
-    { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 } //  x: 4, y: 4 apre sotto,
+    { x: 1, y: 4 }, { x: 2, y: 4 }, { x: 3, y: 4 }, //  x: 4, y: 4 apre sotto,
 	  // Secondo quadrato
     { x: 6, y: 1 }, { x: 8, y: 1 },
     { x: 6, y: 2 }, { x: 8, y: 2 },
